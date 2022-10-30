e488b6cb33e8c5227aafad16a4494bd8a0906cb1
Ashutosh5333 <lakshakarashutosh@gmail.com>
2022-10-30 17:02:38 +0530

    av

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, Navbar1 } from "./Components/Navbar/Navbar";
+
+function App() {
+
+  return (
+    <div className="App">
+         <Navbar/>
+         <Navbar1/>
+    </div>
+  )
+  
+}
+
+export default App;
